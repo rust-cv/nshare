d691953feb8fceabb76d775e3c90ae2629ab8236
nicolo-lucchesi <nicolo.lucchesi@hexagon.com>
2024-03-15 23:58:26 +0100

    draft: Array2->GrayImage Array3->Rgb(a)Image

--- src/toimage.rs
+++ src/toimage.rs
@@ -1,3 +1,6 @@
+#[cfg(feature = "image")]
+mod ndarray_impl;
+
 /// Converts a 2d type to a luma image type.
 ///
 /// This uses an associated type to avoid ambiguity for the compiler.
@@ -6,4 +9,34 @@ pub trait ToImageLuma {
     type Out;
 
     fn into_image_luma(self) -> Self::Out;
+}
+
+/// Converts a 3d array type to an RgbaImage type.
+///
+/// This uses an associated type to avoid ambiguity for the compiler.
+/// By calling this, the compiler always knows the returned type.
+pub trait ToImageRgba {
+    type Out;
+
+    fn into_image_rgba(self) -> Self::Out;
+}
+
+/// Converts a 3d array type to an RgbaImage type.
+///
+/// This uses an associated type to avoid ambiguity for the compiler.
+/// By calling this, the compiler always knows the returned type.
+pub trait ToImageRgb {
+    type Out;
+
+    fn into_image_rgb(self) -> Self::Out;
+}
+
+/// Converts a 2d array type to a GrayImage type.
+///
+/// This uses an associated type to avoid ambiguity for the compiler.
+/// By calling this, the compiler always knows the returned type.
+pub trait ToImageGray {
+    type Out;
+
+    fn into_image_gray(self) -> Self::Out;
 }
